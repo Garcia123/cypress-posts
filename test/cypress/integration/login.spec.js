8eac9a23e9a544fe155e5e73c7beb7744887d2df
Jose Garcia <newmoon798@gmail.com>
2020-03-05 00:47:43 -0500

    jose garcia peceros

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -1,17 +1,22 @@
 
 
-
 describe('Pruebas de login', () =>{
+    
+    before(() => {
+        cy.exec('npm run test:clean');
+    });
 
     beforeEach(() => {
         cy.fixture('users.json').as('userData');
         cy.visit('/login');
         cy.contains('h1','Bienvenido').should('be.visible');
     });
+
     // skip
-    it.skip('crear una cuenta', async ()=> {
+    it('debe registrar un usuario', ()=> {
+
         cy.get('@userData').then(user => {
-            cy.get('.extras > :nth-child(2)').click();
+            cy.get('#login > section > div.col2 > form > div > a:nth-child(2)').click();
             cy.get('#name').type(user.name);
             cy.get('#title').type(user.conpany);
             cy.get('#email2').type(user.email);
@@ -20,27 +25,20 @@ describe('Pruebas de login', () =>{
             cy.wait(3000);
             cy.get('.error-msg').should('not.exist');
         });
-        // cy.contains("Crear una cuenta")
         
     });
 
     it('debe fallar con un usuario erroneo', ()=> {
 
         // cy.contains("Crear una cuenta")
-        cy.get('#email1').type("jose@jose.com");
-        cy.get('#password1').type("test1234");
-        cy.contains('.button',"Ingresar").click();
-        cy.wait(3000);
+        cy.loginUser('falil@test.com','test1234');
         cy.get('.error-msg').should('be.visible');
         
     });
 
     it('debe logearse un usuario', ()=> {
         cy.get('@userData').then(user => {
-            cy.get('#email1').type(user.email);
-            cy.get('#password1').type(user.psswrod);
-            cy.contains('.button',"Ingresar").click();
-            cy.wait(3000);
+            cy.loginUser(user.email, user.psswrod);
             cy.contains('a', 'Dashboard').should('be.visible');
         });
         // cy.contains("Crear una cuenta")
--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginUser', (userName, password) => {
+    cy.get('#email1').type(userName);
+    cy.get('#password1').type(password);
+    cy.contains('.button',"Ingresar").click();
+    cy.wait(3000);
+});
